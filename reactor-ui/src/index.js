240899cc739ed442d465b9af2a5f2223f296afa3
Warren Mira <warrenmira@gmail.com>
2016-12-17 10:35:14 +0800

    added icons kit

--- reactor-ui/src/index.js
+++ reactor-ui/src/index.js
@@ -1,50 +1,50 @@
-/* jshint -W097, esnext:true, unused: vars */
+import 'babel-polyfill';
+import React, { PropTypes } from 'react';
+import { render } from 'react-dom';
 
-import RSideNav from 'reactor-ui/sidenav';
+import { SideNav } from 'reactor-ui/sidenav';
+import { Router, Route, hashHistory } from 'react-router';
 
-import React from 'react';
-import Router from 'react-router';
+import { Buttons } from './buttons/Buttons';
 
-const { SideNav } = RSideNav;
-const { Route, DefaultRoute, RouteHandler, Link } = Router;
-
-
-var navi = [
-    { id: 'home', icon: "fa fa-home" , text: "Home"},
-    { id: 'buttons', icon: "fa fa-sliders", text: "Button/Controls/Toggles"},
-    { id: 'grid', icon: "fa fa-server", text: "Grid System"},
-    { id: 'tabs', icon: 'fa fa-tablet' ,text: "Tabs"},
-    { id: 'panels', icon: 'fa fa-columns' ,text: "Panels"},
-    { id: 'sidenav', icon: 'fa fa-circle-thin' ,text: "Side Navigation"},
-    { id: 'tables', icon: 'fa fa-table' ,text: "Table"},
-    { id: 'github', icon: 'fa fa-github' ,text: "GitHub Page"}
+const navi = [
+    { id: 'home', icon: 'fa fa-home' , text: 'Home'},
+    { id: 'buttons', icon: 'fa fa-sliders', text: 'Button/Controls/Toggles'},
+    { id: 'tabs', icon: 'fa fa-tablet' ,text: 'Tabs'},
+    { id: 'panels', icon: 'fa fa-columns' ,text: 'Panels'},
+    { id: 'sidenav', icon: 'fa fa-circle-thin' ,text: 'Side Navigation'},
+    { id: 'tables', icon: 'fa fa-table' ,text: 'Table'},
+    { id: 'github', icon: 'fa fa-github' ,text: 'GitHub Page'}
 ];
 
-
-
+var selected = 'home';
 var sideNavListener = function(selection) {
-    window.location.hash = selection.id;
+    selected = selection.id;
+    hashHistory.push(selected);
+    render(<SideNav onSelection={sideNavListener} selected={selected} navs={navi}/>, document.getElementById('sidenav'));
 };
 
 var App = React.createClass({
+    propTypes:{
+        children: PropTypes.node
+    },
 
     render() {
-        return <RouteHandler/>;
+        return <span>{this.props.children}</span>;
     }
 
 });
 
 
-var routes = (
-    <Route handler={App}>
-        <Route name="buttons" path="buttons" handler={require("./Buttons")} />
-        <Route name="panels" path="panels" handler={require("./Panels")} />
-    </Route>
+var Routes = (
+
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <Route path="buttons" component={Buttons}/>
+        </Route>
+    </Router>
 );
 
-Router.run(routes, function (Handler) {
-    React.render(<Handler/>, document.getElementById('main'));
-});
 
-
-React.render(<SideNav onSelection={sideNavListener} navs={navi}/>, document.getElementById('sidenav'));
+render(Routes, document.getElementById('main'));
+render(<SideNav onSelection={sideNavListener} selected={selected} navs={navi}/>, document.getElementById('sidenav'));
--- reactor-ui/src/webpack.config.js
+++ reactor-ui/src/webpack.config.js
@@ -1,34 +1,40 @@
 var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('reactor-ui/cssvars');
+var autoprefixer = require('autoprefixer');
+var vars = require('postcss-simple-vars');
+var varsFunc = () => {
+    return cssvars;
+};
 
 
-var plugins = [
+var plugins = [];
 
-];
-
-if (process.env.COMPRESS) {
-
-    plugins.push(
-
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                warnings: false
-            }
-        })
-    );
-}
 
 module.exports = {
-
-    node: {
-        buffer: false
-    },
+    devtool: 'source-map',
 
     plugins: plugins,
 
     module: {
         loaders: [
-            { test: /\.js$/,  loader: "babel-loader"}
+            { test: /\.css$/,loader: 'style-loader!css-loader?localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'},
+            {test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react', 'stage-2']
+                }
+            }
         ]
-    }
+    },
+    postcss: function () {
+        return [vars({ variables: varsFunc, silent: true}), autoprefixer];
+    },
 
+    resolve: {
+        alias: {
+            react: __dirname + '/node_modules/react'
+        }
+    }
 };
